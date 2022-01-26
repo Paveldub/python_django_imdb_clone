be85766fe8186f7ed7ca20013c4c4e38a36991b4
pavel.dzemidovich <dubstr1@gmail.com>
2022-01-26 15:44:39 +0300

    custom permissions

--- watchmate/urls.py
+++ watchmate/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('watch/', include('watchlist_app.api.urls')),
+    
+    # temporary user auth
     path('api-auth/', include('rest_framework.urls'))
 ]
