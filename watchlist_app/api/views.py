50594982d2059d15c23a4ee0e5b4282e9c0c7ee1
pavel.dzemidovich <dubstr1@gmail.com>
2022-01-26 15:36:48 +0300

    custom permissions

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,6 +1,5 @@
 # rest framework imports
-from platform import platform
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 # for mixins and generics
 from rest_framework import mixins
@@ -10,17 +9,22 @@ from rest_framework import viewsets
 from watchlist_app.models import WatchList, StreamPlatform, Review
 from watchlist_app.api.serializers import WatchListSerializer, StreamPlatformSerializer, ReviewSerializer
 
+# PERMISSIONS 
+from watchlist_app.api.permissions import AdminOrReadOnly
+
 # lists
 # Concrete View Classes
 class ReviewVeiwSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = Review.objects.all() 
     serializer_class = ReviewSerializer
     # if user auth show this data
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AdminOrReadOnly]
         
 class WatchViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
     queryset = WatchList.objects.all()
     serializer_class = WatchListSerializer
+     # if user auth show this data
+    permission_classes = [IsAuthenticated]
 
 class StreamPlatformViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin,  viewsets.GenericViewSet):
     queryset = StreamPlatform.objects.all()
